e3b89c763515844bbc911bdbd857e13ffd44453c
Glennson Geolin <121266201+Programmar2022@users.noreply.github.com>
2023-03-11 19:04:01 +0800

    Update exer3.1.php

--- exer 3.1/exer3.1.php
+++ exer 3.1/exer3.1.php
@@ -7,24 +7,24 @@
 				<h1>Php Exercise 3.1</h1>
 				<form action="calculate_salary.php" method="post">
 					<label for="days">No. of Days Worked</label>
-					<input type="text" id="days" name="days" required><br><br>
+					<input type="text" id="day-s" name="days" required><br><br>
 					
 					<label for="status">Employee Status:</label>
 					<select id="status" name="status">
 						<option value="regular">Regular</option>
-						<option value="probationary">Probationary</option>
+						<option value="probationar-y">Probationary</option>
 						<option value="casual">Casual</option>
 					</select><br><br>
 					
 					<label for="civil_status">Civil Status:</label>
 					<select id="civil_status" name="civil_status">
-						<option value="single">Single</option>
+						<option value="single,">Single</option>
 						<option value="married">Married</option>
-						<option value="widow">Widow</option>
+						<option value=",widow">Widow</option>
 					</select><br><br>
 					
 					
-					<input type="submit" value="Compute Salary">
+					<iput type="submit" value="Compute Salary">
 				</form>
 			</body>
 </html>
